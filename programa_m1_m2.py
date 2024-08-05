f08db3f2ce5f29d86a00567fa63a437b538e2e6b
pfsq2001 <132702952+pfsq2001@users.noreply.github.com>
2024-08-05 14:20:46 -0300

    Update programa_m1_m2.py

--- programa_m1_m2.py
+++ programa_m1_m2.py
@@ -61,7 +61,7 @@ if target is not None:
       st.write("M2 5' 3':\n", m2)
       st.write("Alvo 5' 3':\n", alvo)
 
-    elif complemento == "5' -> 3'":
+    elif complemento == "3' -> 5'":
       a = []
       b = []
       c= []
